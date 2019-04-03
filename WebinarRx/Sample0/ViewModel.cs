19ab01942a1a8d807ee5788fd08a2187c3f71bcb
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-04-03 12:18:34 +0200

    Add project files.

---
+++ WebinarRx/Sample0/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using ReactiveUI;
+
+namespace WebinarRx.Sample0
+{
+    public class ViewModel : ReactiveObject
+    {
+        private string firstName;
+        private string lastName;
+        private readonly ObservableAsPropertyHelper<string> fullName;
+
+        public ViewModel()
+        {
+            fullName = this
+                .WhenAnyValue(x => x.FirstName, x => x.LastName, (f, l) => $"{f} {l}")
+                .ToProperty(this, x => x.FullName);
+        }
+
+        public string FullName => fullName.Value;
+
+        public string FirstName
+        {
+            get => firstName;
+            set => this.RaiseAndSetIfChanged(ref firstName, value);
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set => this.RaiseAndSetIfChanged(ref lastName, value);
+        }
+    }
+}
---
+++ WebinarRx/Sample2/ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reactive;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace WebinarRx.Sample2
+{
+    public class ViewModel : ReactiveObject
+    {
+        private string message;
+
+        public ViewModel()
+        {
+            DoSomethingCommand = ReactiveCommand.CreateFromTask(DoSomethingAsync);
+            DoSomethingCommand.Subscribe(_ => { this.Message = "Finished"; });
+        }
+
+        public string Message
+        {
+            get => message;
+            set => this.RaiseAndSetIfChanged(ref message, value);
+        }
+
+        public ReactiveCommand<Unit, Unit> DoSomethingCommand { get; }
+
+        private Task DoSomethingAsync()
+        {
+            return Task.Delay(1000);
+        }
+    }
+}
