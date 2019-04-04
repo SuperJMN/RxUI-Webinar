551ca465fdba9b502404bba17f5f66163c6ac371
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-04-05 00:04:45 +0200

    Sample3

---
+++ WebinarRx/Sample0/SimplestViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ReactiveUI;
+
+namespace WebinarRx.Sample0
+{
+    public class SimplestViewModel : ReactiveObject
+    {
+        public SimplestViewModel()
+        {
+            IObservable<string> nameObservable = this.WhenAnyValue(x => x.Name);
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get => name;
+            set => this.RaiseAndSetIfChanged(ref name, value);
+        }
+    }
+}
---
+++ WebinarRx/Sample3/ViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using ReactiveUI;
+
+namespace WebinarRx.Sample3
+{
+    public class ViewModel : ReactiveObject
+    {
+        private string url;
+        private readonly ObservableAsPropertyHelper<string> html;
+
+        public ViewModel()
+        {
+            var canLoad = this.WhenAnyValue(x => x.Url).Select(x => !string.IsNullOrEmpty(x));
+            LoadUrlCommand = ReactiveCommand.CreateFromTask(() => LoadHtml(Url), canLoad);
+            html = LoadUrlCommand.ToProperty(this, x => x.Html);
+
+            LoadUrlCommand
+                .ThrownExceptions
+                .Subscribe(async exception => await new MessageDialog("Cannot load the website", "Error").ShowAsync());
+        }
+
+        private static async Task<string> LoadHtml(string urlToLoad)
+        {
+            using (var client = new HttpClient())
+            {
+                return await client.GetStringAsync(urlToLoad);
+            }
+        }
+
+        public ReactiveCommand<Unit, string> LoadUrlCommand { get; }
+
+        public string Html => html.Value;
+
+        public string Url
+        {
+            get => url;
+            set => this.RaiseAndSetIfChanged(ref url, value);
+        }
+    }
+}
