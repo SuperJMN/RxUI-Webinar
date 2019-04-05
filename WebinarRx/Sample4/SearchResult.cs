afb6f885856044c65666e1cd920f4fd204697e7e
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-04-05 10:06:43 +0200

    Sample4

---
+++ WebinarRx/Sample4/SearchResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebinarRx.Sample4
+{
+    public class SearchResult
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string DisplayUrl { get; set; }
+    }
+}
---
+++ WebinarRx/Sample4/ViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Search.WebSearch;
+using ReactiveUI;
+
+namespace WebinarRx.Sample4
+{
+    public class ViewModel : ReactiveObject
+    {
+        private readonly WebSearchClient client;
+        private IEnumerable<SearchResult> searchResults;
+        private string searchQuery;
+
+        public ViewModel()
+        {
+            client = new WebSearchClient(new ApiKeyServiceClientCredentials("f891515b49d94b64aa91955aa92c1691"));
+
+            this.WhenAnyValue(x => x.SearchQuery)
+                .Throttle(TimeSpan.FromSeconds(0.8), RxApp.TaskpoolScheduler)
+                .Select(query => query?.Trim())
+                .DistinctUntilChanged()
+                .Where(query => !string.IsNullOrWhiteSpace(query))
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .InvokeCommand(ExecuteSearch);
+
+            ExecuteSearch = ReactiveCommand.CreateFromTask(() => Search(SearchQuery));
+            ExecuteSearch.Subscribe(z => SearchResults = z);
+        }
+
+        public ReactiveCommand<Unit, IEnumerable<SearchResult>> ExecuteSearch { get; }
+
+
+        public IEnumerable<SearchResult> SearchResults
+        {
+            get => searchResults;
+            set => this.RaiseAndSetIfChanged(ref searchResults, value);
+        }
+
+        public string SearchQuery
+        {
+            get => searchQuery;
+            set => this.RaiseAndSetIfChanged(ref searchQuery, value);
+        }
+
+        private async Task<IEnumerable<SearchResult>> Search(string s)
+        {
+            var result = await client.Web.SearchAsync(s);
+            var results = result.WebPages.Value.Select(x => new SearchResult
+            {
+                DisplayUrl = x.DisplayUrl,
+                Url = x.Url,
+                Title = x.Text,
+                Description = x.Description
+            });
+
+            return results;
+        }
+    }
+}
